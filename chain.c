37b7e6dec8df16e3affa8a310bcffccb1abdb6ec
Simphiwe-Sithebe <108188116+Simphiwe-Sithebe@users.noreply.github.com>
2023-09-21 02:31:14 +0200

    Update chain.c

--- chain.c
+++ chain.c
@@ -2,13 +2,13 @@
 
 /**
  * is_chain - test if current char in buffer is a chain delimeter
- * @infostruct: the parameter struct
+ * @inf: the parameter struct
  * @charbuf: the char buffer
  * @p: address of current position in charbuf
  *
  * Return: 1 if chain delimeter, 0 otherwise
  */
-int is_chain(info_t *infostruct, char *charbuf, size_t *p)
+int is_chain(info_t *inf, char *charbuf, size_t *p)
 {
 	size_t j = *p;
 
@@ -16,18 +16,18 @@ int is_chain(info_t *infostruct, char *charbuf, size_t *p)
 	{
 		charbuf[j] = 0;
 		j++;
-		infostruct->cmd_buf_type = CMD_OR;
+		inf->cmd_buf_type = CMD_OR;
 	}
 	else if (charbuf[j] == '&' && charbuf[j + 1] == '&')
 	{
 		charbuf[j] = 0;
 		j++;
-		infostruct->cmd_buf_type = CMD_AND;
+		inf->cmd_buf_type = CMD_AND;
 	}
 	else if (charbuf[j] == ';') /* found end of this command */
 	{
 		charbuf[j] = 0; /* replace semicolon with null */
-		infostruct->cmd_buf_type = CMD_CHAIN;
+		inf->cmd_buf_type = CMD_CHAIN;
 	}
 	else
 		return (0);
@@ -37,7 +37,7 @@ int is_chain(info_t *infostruct, char *charbuf, size_t *p)
 
 /**
  * check_chain - checks we should continue chaining based on last status
- * @infostruct: the parameter struct
+ * @inf: the parameter struct
  * @charbuf: the char buffer
  * @p: address of current position in charbuf
  * @i: starting position in charbuf
@@ -45,21 +45,21 @@ int is_chain(info_t *infostruct, char *charbuf, size_t *p)
  *
  * Return: Void
  */
-void check_chain(info_t *infostruct, char *charbuf, size_t *p, size_t i, size_t len)
+void check_chain(info_t *inf, char *charbuf, size_t *p, size_t i, size_t len)
 {
 	size_t j = *p;
 
-	if (infostruct->cmd_buf_type == CMD_AND)
+	if (inf->cmd_buf_type == CMD_AND)
 	{
-		if (infostruct->status)
+		if (inf->status)
 		{
 			charbuf[i] = 0;
 			j = len;
 		}
 	}
-	if (infostruct->cmd_buf_type == CMD_OR)
+	if (inf->cmd_buf_type == CMD_OR)
 	{
-		if (!infostruct->status)
+		if (!inf->status)
 		{
 			charbuf[i] = 0;
 			j = len;
@@ -71,11 +71,11 @@ void check_chain(info_t *infostruct, char *charbuf, size_t *p, size_t i, size_t 
 
 /**
  * replace_alias - replaces an aliases in the tokenized string
- * @infostruct: the parameter struct
+ * @inf: the parameter struct
  *
  * Return: 1 if replaced, 0 otherwise
  */
-int replace_alias(info_t *infostruct)
+int replace_alias(info_t *inf)
 {
 	int i;
 	list_t *node;
@@ -83,57 +83,57 @@ int replace_alias(info_t *infostruct)
 
 	for (i = 0; i < 10; i++)
 	{
-		node = node_starts_with(infostruct->alias, infostruct->argv[0], '=');
+		node = node_starts_with(inf->alias, inf->argv[0], '=');
 		if (!node)
 			return (0);
-		free(infostruct->argv[0]);
+		free(inf->argv[0]);
 		p = _strchr(node->str, '=');
 		if (!p)
 			return (0);
 		p = _strdup(p + 1);
 		if (!p)
 			return (0);
-		infostruct->argv[0] = p;
+		inf->argv[0] = p;
 	}
 	return (1);
 }
 
 /**
  * replace_vars - replaces vars in the tokenized string
- * @infostruct: the parameter struct
+ * @inf: the parameter struct
  *
  * Return: 1 if replaced, 0 otherwise
  */
-int replace_vars(info_t *infostruct)
+int replace_vars(info_t *inf)
 {
 	int i = 0;
 	list_t *node;
 
-	for (i = 0; infostruct->argv[i]; i++)
+	for (i = 0; inf->argv[i]; i++)
 	{
-		if (infostruct->argv[i][0] != '$' || !infostruct->argv[i][1])
+		if (inf->argv[i][0] != '$' || !inf->argv[i][1])
 			continue;
 
-		if (!_strcmp(infostruct->argv[i], "$?"))
+		if (!_strcmp(inf->argv[i], "$?"))
 		{
-			replace_string(&(infostruct->argv[i]),
-				_strdup(convert_number(infostruct->status, 10, 0)));
+			replace_string(&(inf->argv[i]),
+				_strdup(convert_number(inf->status, 10, 0)));
 			continue;
 		}
-		if (!_strcmp(infostruct->argv[i], "$$"))
+		if (!_strcmp(inf->argv[i], "$$"))
 		{
-			replace_string(&(infostruct->argv[i]),
+			replace_string(&(inf->argv[i]),
 				_strdup(convert_number(getpid(), 10, 0)));
 			continue;
 		}
-		node = node_starts_with(infostruct->env, &infostruct->argv[i][1], '=');
+		node = node_starts_with(inf->env, &inf->argv[i][1], '=');
 		if (node)
 		{
-			replace_string(&(infostruct->argv[i]),
+			replace_string(&(inf->argv[i]),
 				_strdup(_strchr(node->str, '=') + 1));
 			continue;
 		}
-		replace_string(&infostruct->argv[i], _strdup(""));
+		replace_string(&inf->argv[i], _strdup(""));
 
 	}
 	return (0);
